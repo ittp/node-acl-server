873ffc04a340991cced6e4b6ccfa9763c493ae03
ittp <ittp.spb@gmail.com>
2022-05-16 00:22:05 +0300

    2 files modified, 2 added

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 // run `node index.js` in the terminal
 
 console.log(`Hello Node.js v${process.versions.node}!`);
+
+class ACL {}
+let debug = true;
+let isDebug = debug == true;
+
+class Config {
+  render() {
+    console.log('render');
+
+    return { key: 1, data: '1' };
+  }
+}
